f984d00ef88d95816094f42857381a9a854c3e6b
Artur Kulembetov <artur_kulembetov@mail.ru>
2023-04-06 17:32:42 +0500

    refactoring: id changed, css classes renamed, new events and logo added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import "../src/index.css";
-const tab = document.querySelectorAll(".tab");
+import '../src/index.css';
+const section = document.querySelectorAll('#section');
 
-tab.forEach((item,) => {
-  item.addEventListener("click", evt => {
-    item.classList.toggle("tab_active");
-    // item.classList.remove("tab_active");
-    // evt.target.classList.add("tab_active");
+section.forEach((item) => {
+  item.addEventListener('click', () => {
+    const items = Array.from(document.querySelectorAll('#section'));
+    items.map((item) => item.classList.remove('tab_active'));
+    item.classList.toggle('tab_active');
   });
 });
 
+const link = document.querySelectorAll('#link');
 
+link.forEach((item) => {
+  item.addEventListener('click', () => {
+    const items = Array.from(document.querySelectorAll('#link'));
+    items.map((item) => item.classList.remove('tab_active'));
+  });
+  item.addEventListener('mouseup', () => {
+    item.classList.remove('tab_active');
+  });
+  item.addEventListener('mousedown', () => {
+    item.classList.add('tab_active');
+  });
+});
