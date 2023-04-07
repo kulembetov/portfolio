88740b49a22f76f9634a8a4bee660ac56c57ce66
Artur Kulembetov <artur_kulembetov@mail.ru>
2023-04-07 23:37:20 +0500

    constants were moved to own folder, added comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import '../src/index.css';
-const section = document.querySelectorAll('#section');
-const link = document.querySelectorAll('#link');
+import {
+  section,
+  link,
+  arrowLeft,
+  arrowRight,
+  arrowUp,
+  arrowDown,
+} from './utils/constants.js';
+
+// Listener on tabs
 
 section.forEach((item) => {
   item.addEventListener('click', () => {
@@ -24,4 +32,62 @@ link.forEach((item) => {
   item.addEventListener('mouseleave', () => {
     item.classList.remove('tab_active');
   });
+});
+
+// Effect pressing the button
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.code === 'ArrowLeft') {
+    arrowLeft.style.boxShadow = 'none';
+    arrowLeft.style.transform = 'translateY(4)';
+  }
+});
+
+document.addEventListener('keyup', (evt) => {
+  if (evt.code === 'ArrowLeft') {
+    arrowLeft.style.boxShadow = '0 3px #24856d';
+    arrowLeft.style.transform = 'translateY(0)';
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.code === 'ArrowRight') {
+    arrowRight.style.boxShadow = 'none';
+    arrowRight.style.transform = 'translateY(4)';
+  }
+});
+
+document.addEventListener('keyup', (evt) => {
+  if (evt.code === 'ArrowRight') {
+    arrowRight.style.boxShadow = '0 3px #24856d';
+    arrowRight.style.transform = 'translateY(0)';
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.code === 'ArrowUp') {
+    arrowUp.style.boxShadow = 'none';
+    arrowUp.style.transform = 'translateY(4)';
+  }
+});
+
+document.addEventListener('keyup', (evt) => {
+  if (evt.code === 'ArrowUp') {
+    arrowUp.style.boxShadow = '0 3px #24856d';
+    arrowUp.style.transform = 'translateY(0)';
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.code === 'ArrowDown') {
+    arrowDown.style.boxShadow = 'none';
+    arrowDown.style.transform = 'translateY(4)';
+  }
+});
+
+document.addEventListener('keyup', (evt) => {
+  if (evt.code === 'ArrowDown') {
+    arrowDown.style.boxShadow = '0 3px #24856d';
+    arrowDown.style.transform = 'translateY(0)';
+  }
 });
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const section = document.querySelectorAll('#section');
+const link = document.querySelectorAll('#link');
+const arrowLeft = document.querySelector('#arrow-left');
+const arrowRight = document.querySelector('#arrow-right');
+const arrowUp = document.querySelector('#arrow-up');
+const arrowDown = document.querySelector('#arrow-down');
